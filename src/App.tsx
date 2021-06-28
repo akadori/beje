5c1a0c1aad54047c448f200636ed2a672380a1f2
tamaritamari <tamari@minacare.co.jp>
2021-06-28 09:53:19 +0900

    linted

--- src/App.tsx
+++ src/App.tsx
@@ -1,103 +1,122 @@
-import React, { useState, useEffect, useRef, MutableRefObject } from 'react'
+import React, { useState, useEffect, useRef, MutableRefObject } from "react";
 function App() {
   type Point = {
     x: number;
     y: number;
-  }
+  };
   type Line = {
     start: Point;
     end: Point;
-  }
-  const canvas = useRef<HTMLCanvasElement>(null)
-  const [points, setPoints] = useState<Point[]>([])
+  };
+  const canvas = useRef<HTMLCanvasElement>(null);
+  const [points, setPoints] = useState<Point[]>([]);
   const [dimensions, setDimensions] = useState({
     height: window.innerHeight,
-    width: window.innerWidth
-  })
-  const INITIAL = 600
-  const [timeLeft, setTimeLeft] = useState<number>(INITIAL)
+    width: window.innerWidth,
+  });
+  const INITIAL = 600;
+  const [timeLeft, setTimeLeft] = useState<number>(INITIAL);
   useEffect(() => {
-    const timeId = setInterval(() => setTimeLeft((tl) => tl === 0 ? INITIAL : tl - 1), 10)
-    return () => clearInterval(timeId)
-  }, [])
+    const timeId = setInterval(
+      () => setTimeLeft((tl) => (tl === 0 ? INITIAL : tl - 1)),
+      10
+    );
+    return () => clearInterval(timeId);
+  }, []);
 
   const connectPoints = (points: Point[]): Line[] => {
     return points.reduce((lines: Line[], p, index, array) => {
-      return index > 0 ? [...lines, { start: array[index - 1], end: p }] : lines
-    }, [])
-  }
+      return index > 0
+        ? [...lines, { start: array[index - 1], end: p }]
+        : lines;
+    }, []);
+  };
 
   const pointsOnLines = (lines: Line[], t: number) => {
-    return lines.map(l => {
-      const { start, end } = l
-      return { x: start.x + (end.x - start.x) * t, y: start.y + (end.y - start.y) * t }
-    })
-  }
+    return lines.map((l) => {
+      const { start, end } = l;
+      return {
+        x: start.x + (end.x - start.x) * t,
+        y: start.y + (end.y - start.y) * t,
+      };
+    });
+  };
 
   const pointsAndLines = (clickedPoints: Point[], t: number) => {
-    const len = clickedPoints.length
-    const result: {points: Point[], lines: Line[]} = {points: [], lines: []}
-    let targetPoints = clickedPoints
+    const len = clickedPoints.length;
+    const result: { points: Point[]; lines: Line[] } = {
+      points: [],
+      lines: [],
+    };
+    let targetPoints = clickedPoints;
     for (let index = 0; index < len; index++) {
-      result.points.push(...targetPoints)
+      result.points.push(...targetPoints);
       const lines = connectPoints(targetPoints);
-      result.lines.push(...lines)
-      targetPoints = pointsOnLines(lines, t)
+      result.lines.push(...lines);
+      targetPoints = pointsOnLines(lines, t);
     }
-    return result
-  }
+    return result;
+  };
 
-  const draw = (points: Point[], lines: Line[], canvas: HTMLCanvasElement) =>{
-    const ctx = canvas.getContext('2d')!;
+  const draw = (points: Point[], lines: Line[], canvas: HTMLCanvasElement) => {
+    const ctx = canvas.getContext("2d")!;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = "green"
-    points.forEach(p => {
+    ctx.fillStyle = "green";
+    points.forEach((p) => {
       ctx.beginPath();
-      ctx.arc(p.x, p.y, 5, 0, 360)
-      ctx.stroke()
-    })
+      ctx.arc(p.x, p.y, 5, 0, 360);
+      ctx.stroke();
+    });
     lines.forEach((l) => {
-      ctx.beginPath()
-      ctx.moveTo(l.start.x, l.start.y)
-      ctx.lineTo(l.end.x, l.end.y)
-      ctx.stroke()
-    })
-  }
+      ctx.beginPath();
+      ctx.moveTo(l.start.x, l.start.y);
+      ctx.lineTo(l.end.x, l.end.y);
+      ctx.stroke();
+    });
+  };
 
   useEffect(() => {
     if (canvas.current) {
-      const {points: pointsToDraw, lines} = pointsAndLines(points, timeLeft/INITIAL)
-      draw(pointsToDraw, lines, canvas.current)
+      const { points: pointsToDraw, lines } = pointsAndLines(
+        points,
+        timeLeft / INITIAL
+      );
+      draw(pointsToDraw, lines, canvas.current);
     }
-  }, [points, timeLeft])
-  
+  }, [points, timeLeft]);
+
   useEffect(() => {
     function handleResize() {
       setDimensions({
         height: window.innerHeight,
-        width: window.innerWidth
-      })
-
+        width: window.innerWidth,
+      });
     }
 
-    window.addEventListener('resize', handleResize)
-  })
-
-
+    window.addEventListener("resize", handleResize);
+  });
 
   const onClickHandle = (e: React.MouseEvent<HTMLCanvasElement>) => {
-    e.preventDefault()
+    e.preventDefault();
     if (canvas.current) {
-      setPoints((prevPoints) => [...prevPoints, { x: e.clientX, y: e.clientY }])
+      setPoints((prevPoints) => [
+        ...prevPoints,
+        { x: e.clientX, y: e.clientY },
+      ]);
     }
-  }
+  };
 
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
       <p>timeleft: {timeLeft}</p>
-      <canvas {...dimensions} style={{ "borderColor": "red", position: "absolute", top: 0, left: 0 }} ref={canvas} onClick={onClickHandle}></canvas>
+      <canvas
+        {...dimensions}
+        style={{ borderColor: "red", position: "absolute", top: 0, left: 0 }}
+        ref={canvas}
+        onClick={onClickHandle}
+      ></canvas>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
